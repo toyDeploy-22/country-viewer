17793f19c079b97c5fa2512885c6698196480c4b
Eelismey-jf <eelismey@gmail.com>
2025-03-28 23:41:14 +0100

    rectifier

---
+++ countryServer/NoSQL/countryModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const continentsAbbr = ;
+
+const continentSchema = new mongoose.Schema({
+  continentId: {type: String, 
+  maxLength: 2, 
+  enum: {
+	  values: ["NA", "SA", "EU", "AF", "AS", "OC"],
+	  message: "The continent is not recognized. Make sure the continent is a listed abbreviation."
+  }, 
+  required: [true, "The continent ID is mandatory."}
+});
+
+const countrySchema = new mongoose.Schema({
+	
+ countryId: {
+	 type: String, uppercase: true, minLength: [2, "The country ID must contain at least two characters."], maxLength: [6, "The country ID cannot contain more than 6 characters."], required: [true, "country ID is required."]
+ },
+ 
+ countryName: {
+	 type: String, minLength: [2, "A country name must contain at least more than one letter."], maxLength: [40, "The country Name can contain 40 characters maximum."], required: [true, "The country Name is required."]
+	 },
+	 
+ countryFlag_url: {type: String, required: false}, 
+ 
+ countryDescription: {
+	 type: String, minLength: [10, "If filled-in, the country Description must at least contain a sentence of 10 characters. You can leave this field empty if you want."], maxLength: [250, "The country Description cannot contain more than 250 characters."], required: false
+	 },
+ 
+ continent: {
+ continentId: [continentSchema]
+});
+
+const countryModel = mongoose.model('Countrie', countrySchema);
+
+export default countryModel; 
--- countryServer/app.js
+++ countryServer/app.js
@@ -1,15 +1,29 @@
+// Core
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
+// 3rd party
 import Express from "express";
 import cors from "cors";
-import countryPoolAsync from "./myCountryPool.js";
-import myCountryRoutes from "./myCountryRoutes.js";
+
+// Local
+import countryPoolAsync from "./NoSQL/myCountryPool.js";
+// import myCountryRoutes from "./NoSQL/myCountryRoutes.js";
+/*
+import countryPoolAsync from "./SQL/myCountryPool.js";
+import myCountryRoutes from "./SQL/myCountryRoutes.js";
+*/
+
 
 // Variables
 const myServer = Express();
+let connectionData = new Object();
+const {MY_PORT, MY_MONGO_LOCAL} = process.env;
+const successPage = join(dirname(fileURLToPath(import.meta.url)), "htmlSuccessPage.html");
 
-const myPort = process.env.MY_PORT || 5000;
+// const myPort = process.env.MY_PORT || 5000;
 
-
+/*
 const countryPoolConn = async() => {
 	try {
 		const conn = await countryPoolAsync.getConnection();
@@ -19,21 +33,42 @@ const countryPoolConn = async() => {
 		console.error("Countries database connection failed:", err.stack)
 	}
 }
-
+*/
 
 // Middlewares
 myServer.use(Express.json());
 myServer.use(cors());
-myServer.use("/", myCountryRoutes);
+// myServer.use("/sql", myCountryRoutes);
+// myServer.use("/nosql", myCountryRoutes);
+
+myServer.get("/", (req, res) => {
+	try {
+		if(connectionData.hasOwnProperty('db_Host')) {
+		console.log("DB data collection successful");
+		for (const [k, v] of Object.entries(connectionData)) {
+			res.setHeader(k, v)
+			}
+		res.sendFile(successPage)
+		} else {
+		res.status(400).send("The database could not connect this time. Please try to refresh the page.")
+		}
+	} catch(err) {
+		console.error(err);
+		res.status(500).send("Ooops, something wrong occurs. Please try to connect again and contact your administrator if you see this page back.")
+		}
+})
 
 // Method
-await countryPoolConn();
+// await countryPoolConn();
 
-myServer.listen(myPort, ()=>
-console.log("server connected !")
-)
+myServer.listen(MY_PORT || 5000, async() => {
+	console.log("server connected !");
+	const conn = await countryPoolAsync(MY_MONGO_LOCAL);
+	connectionData = {...conn} // overwrite
+	console.log("Database connected !")
+})
 
 myServer.on("error", 
-            (err)=>console.error("server not connected:", err)
+            (err) => console.error("ErrorMessage-Event: ", err.message)
            )
 
