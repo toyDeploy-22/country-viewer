0fc1e2a65525a311375ee3354969a53faa7c5f03
Eelismey-jf <eelismey@gmail.com>
2025-05-22 05:26:46 +0200

    rectifier

--- countryPage/src/components/NavBar.jsx
+++ countryPage/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+// import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouseFlag } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
